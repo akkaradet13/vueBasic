d4eed8bb8bd6528cb40d75ba6688368502e57c9d
akkaradet13 <akkaradet8.11.41@gmail.com>
2023-07-15 09:59:11 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h1>okok</h1>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data(){
+    return{
+      firstName: "oko",
+      lastName: "naja",
+      age: 10
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
